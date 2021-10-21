ed209475c89b8dd027d610bb2917e54c44264c89
Travis Vachon <travis.vachon@gmail.com>
2021-10-20 17:15:39 -0700

    Add swrlit and get basic login/logout working
    Only support inrupt.net for now, but we'll open this up soon.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { AuthenticationProvider } from 'swrlit'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthenticationProvider>
+      <Component {...pageProps} />
+    </AuthenticationProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useWebId, useLoggedIn, useAuthentication } from 'swrlit'
 
 export default function Home() {
+  const webId = useWebId()
+  const loggedIn = useLoggedIn()
+  const { login, logout } = useAuthentication()
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +18,26 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        {
+          (loggedIn === undefined) ? (
+            <div>loading...</div>
+          ) : (
+            loggedIn ? (
+              <div>
+                <div>welcome to hypey!</div>
+                <button onClick={() => logout()}>log out</button>
+              </div>
+            ) : (
+              <div>
+                <button onClick={() => {
+                  login({ oidcIssuer: "https://inrupt.net", redirectUrl: window.location.href, clientName: "Hypey" })
+                }}>Log in</button>
+              </div>
+            )
+          )
+        }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+      </main >
+    </div >
   )
 }
