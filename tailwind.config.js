fed34cbdb8c391dd024daaf96804515f0d35b3b8
Travis Vachon <travis.vachon@gmail.com>
2021-10-20 19:02:11 -0700

    add tailwind and get the app looking fugly
    not the final design by a long shot, but more fun than all white everything

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        fuchsia: colors.fuchsia
+      }
+    },
+
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
