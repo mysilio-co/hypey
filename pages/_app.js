99241428d83ec928963a6200889de673f05af93e
Travis Vachon <travis.vachon@gmail.com>
2021-10-22 15:43:07 -0700

    upgrade to latest swrlit
    use this to handle onSessionRestore appropriately - doesn't appear to do much right now but will start mattering a lot once we have more pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "cropperjs/dist/cropper.css";
 import '../styles/globals.css'
 import { AuthenticationProvider } from 'swrlit'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
   return (
-    <AuthenticationProvider>
+    <AuthenticationProvider onSessionRestore={url => router.replace(url)}>
       <Component {...pageProps} />
     </AuthenticationProvider>
   )
