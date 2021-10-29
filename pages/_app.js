582f6ae47aa0f63e853e01053b8064bcb2581e04
Travis Vachon <travis.vachon@gmail.com>
2021-10-29 15:56:15 -0700

    use "render after auth" pattern to make auth redirect jiggle less
    we want to make sure we don't render anything before we check auth and potentially redirect.

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,38 @@
 import "cropperjs/dist/cropper.css";
 import '../styles/globals.css'
-import { AuthenticationProvider } from 'swrlit'
+import { AuthenticationProvider, useAuthentication } from 'swrlit'
 import { useRouter } from 'next/router'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
+function RenderAfterAuthed({ children }) {
+  const { info } = useAuthentication()
+
+  return info ? (
+    <>
+    {children}
+    </>
+  ) : (<></>)
+}
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
   return (
-    <AuthenticationProvider onSessionRestore={url => router.replace(url)}>
       <DndProvider backend={HTML5Backend}>
         <Component {...pageProps} />
       </DndProvider>
+  )
+}
+
+function AuthedApp(props) {
+  const router = useRouter()
+  return (
+    <AuthenticationProvider onSessionRestore={url => router.replace(url)}>
+      <RenderAfterAuthed>
+        <MyApp {...props} />
+      </RenderAfterAuthed>
     </AuthenticationProvider>
   )
 }
 
-export default MyApp
+
+export default AuthedApp
